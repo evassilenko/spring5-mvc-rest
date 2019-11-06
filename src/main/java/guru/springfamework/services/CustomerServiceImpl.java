ad9c58252c9b4ed20605273ce7c0125297d5b8db
evassilenko <evassilenko@gmail.com>
2019-11-06 17:06:20 -0500

    adding get customers logic

---
+++ src/main/java/guru/springfamework/services/CustomerServiceImpl.java
@@ -0,0 +1,46 @@
+package guru.springfamework.services;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.springframework.stereotype.Service;
+
+import guru.springfamework.api.v1.mapper.CategoryMapper;
+import guru.springfamework.api.v1.mapper.CustomerMapper;
+import guru.springfamework.api.v1.model.CustomerDTO;
+import guru.springfamework.repositories.CustomerRepository;
+
+@Service
+public class CustomerServiceImpl implements CustomerService {
+
+	private final CustomerMapper customerMapper;
+	private final CustomerRepository customerRepository;
+	
+	
+	public CustomerServiceImpl(CustomerMapper customerMapper, CustomerRepository customerRepository) {
+		super();
+		this.customerMapper = customerMapper;
+		this.customerRepository = customerRepository;
+	}
+
+	@Override
+	public List<CustomerDTO> getAllCustomers() {
+		return customerRepository
+				.findAll()
+				.stream()
+				.map(customer -> {
+					CustomerDTO customerDTO = customerMapper.customerToCustomerDTO(customer);
+					customerDTO.setCustomerUrl("/api/v1/customers/" + customer.getId());
+					return customerDTO;
+				})
+				.collect(Collectors.toList());
+	}
+
+	@Override
+	public CustomerDTO getCustomerById(Long id) {
+		return customerRepository.findById(id)
+				.map(customerMapper::customerToCustomerDTO)
+				.orElseThrow(RuntimeException::new);
+	}
+
+}
---
+++ src/test/java/guru/springfamework/services/CustomerServiceTest.java
@@ -0,0 +1,77 @@
+package guru.springfamework.services;
+
+import static org.junit.Assert.assertEquals;
+import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.Mockito.when;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Optional;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import guru.springfamework.api.v1.mapper.CustomerMapper;
+import guru.springfamework.api.v1.model.CustomerDTO;
+import guru.springfamework.domain.Customer;
+import guru.springfamework.repositories.CustomerRepository;
+
+public class CustomerServiceTest {
+
+    public static final Long ID = 2L;
+    public static final String FIRST_NAME = "Jimmy";
+    public static final String LAST_NAME = "Bow";
+    CustomerService customerService;
+
+    @Mock
+    CustomerRepository customerRepository;
+
+
+    @Before
+    public void setUp() throws Exception {
+        MockitoAnnotations.initMocks(this);
+
+        customerService = new CustomerServiceImpl(CustomerMapper.INSTANCE, customerRepository);
+    }
+
+    @Test
+    public void getAllCategories() throws Exception {
+
+        //given
+        List<Customer> customers = Arrays.asList(new Customer(), new Customer(), new Customer());
+
+        when(customerRepository.findAll()).thenReturn(customers);
+
+        //when
+        List<CustomerDTO> customerDTOS = customerService.getAllCustomers();
+
+        //then
+        assertEquals(3, customerDTOS.size());
+
+    }
+
+    @Test
+    public void getCustomerByID() throws Exception {
+
+        //given
+    	List<Customer> customerList = new ArrayList<Customer>();
+    	Customer customer = new Customer();
+    	customer.setId(ID);
+    	customer.setFirstname(FIRST_NAME);
+    	customer.setLastname(LAST_NAME);
+    	customerList.add(customer);
+
+        when(customerRepository.findById(anyLong())).thenReturn(Optional.of(customer));
+
+        //when
+        CustomerDTO customerDTO = customerService.getCustomerById(ID);
+
+        //then
+        assertEquals(FIRST_NAME, customerDTO.getFirstname());
+        assertEquals(LAST_NAME, customerDTO.getLastname());
+
+    }
+}
